6ec7f948b4e76b04978ad63548fcafb26d1a742d
dorkydhruv <thedhruvsharma@gmail.com>
2024-05-19 01:07:27 +0530

    backend work..

---
+++ backend/db.js
@@ -0,0 +1,32 @@
+import mongoose from "mongoose";
+const {Schema} = mongoose;
+
+mongoose.connect("mongodb+srv://dhruv:1154Dhruv@paytmcluster.enh71c5.mongodb.net/");
+
+const userSchema = new Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        lowercase: true,
+        minlength: 5,
+        maxlength: 20,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    firstName: {
+        type: String,
+        required: true,
+    },
+    lastName: {
+        type: String,
+    },
+})
+
+const User = mongoose.model("User",userSchema);
+module.exports = {
+    User,
+}
--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,12 @@
 const express = require("express");
 
+const app = express();
+app.use(express.json());
+app.use("api/v1/", require("./routes/index.js"));
+app.get("/", (req, res) => {
+    res.send("Hello World");
+});
 
+app.listen(3000,()=>{
+    console.log("Server is running on port 3000");
+})
